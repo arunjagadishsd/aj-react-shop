fe25443abdbcb9b141d6d653e2dc5eedc2034209
arunjagadishsd <arunjagadishsd@gmail.com>
2020-05-16 18:23:43 +0530

    Added signin component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { withRouter } from 'react-router-dom';
+import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { withRouter } from 'react-router-dom'
 
-import CustomButton from '../custom-button/custom-button.component';
-import CartItem from '../cart-item/cart-item.component';
-import { selectCartItems } from '../../redux/cart/cart.selectors';
-import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
+import CustomButton from '../custom-button/custom-button.component'
+import CartItem from '../cart-item/cart-item.component'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js'
 
-import './cart-dropdown.styles.scss';
+import './cart-dropdown.styles.scss'
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
-        cartItems.map(cartItem => (
+        cartItems.map((cartItem) => (
           <CartItem key={cartItem.id} item={cartItem} />
         ))
       ) : (
@@ -22,18 +22,18 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
       )}
     </div>
     <CustomButton
-      onClick={() => {
-        history.push('/checkout');
-        dispatch(toggleCartHidden());
-      }}
+        onClick={() => {
+          history.push('/checkout')
+          dispatch(toggleCartHidden())
+        }}
     >
       GO TO CHECKOUT
     </CustomButton>
   </div>
-);
+)
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
-});
+})
 
-export default withRouter(connect(mapStateToProps)(CartDropdown));
+export default withRouter(connect(mapStateToProps)(CartDropdown))
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,40 +1,42 @@
-import React from 'react';
+import React from 'react'
 
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
-import './sign-in.styles.scss';
+import './sign-in.styles.scss'
 
 class SignIn extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      email: '',
-      password: ''
-    };
+      data: {
+        email: '',
+        password: ''
+      }
+    }
   }
 
-  handleSubmit = async event => {
-    event.preventDefault();
+  handleSubmit = async (event) => {
+    event.preventDefault()
 
-    const { email, password } = this.state;
+    const { email, password } = this.state
 
     try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: '' });
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({ email: '', password: '' })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
-  handleChange = event => {
-    const { value, name } = event.target;
+  handleChange = (event) => {
+    const { value, name } = event.target
 
-    this.setState({ [name]: value });
-  };
+    this.setState({ [name]: value })
+  }
 
   render() {
     return (
@@ -67,8 +69,8 @@ class SignIn extends React.Component {
           </div>
         </form>
       </div>
-    );
+    )
   }
 }
 
-export default SignIn;
+export default SignIn
